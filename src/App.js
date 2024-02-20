2f104404c142f004d9cb4cc069d80585011844fc
kietnguyendevtest <teiknguyen.work@gmail.com>
2024-02-20 13:49:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./assets/scss/main.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>Projects</h1>
+            <ul>
+                <li>
+                    <a href="https://nmkiet.io.vn/" target="_blank">Ecommerce Grocery</a>
+                </li>
+                <li>
+                    <a href="https://kietnm-landing-celobank.vercel.app/" target="_blank">Landing CeloBank</a>
+                </li>
+                <li>
+                    <a href="https://kietnm-landing-besnik.vercel.app/" target="_blank">Landing Besnik</a>
+                </li>
+                <li>
+                    <a href="https://kietnm-landing-dental-care.vercel.app/" target="_blank">Landing Dental Care</a>
+                </li>
+                <li>
+                    <a href="https://kietnm-landing-lesson.vercel.app/" target="_blank">Landing Lesson</a>
+                </li>
+                <li>
+                    <a href="https://kietnm-reactjs.vercel.app/" target="_blank">CRUD ReactJs</a>
+                </li>
+                <li>
+                    <a href="https://kietnm-nextjs.vercel.app" target="_blank">CRUD NextJs</a>
+                </li>
+            </ul>
+        </>
+    );
 }
 
 export default App;
