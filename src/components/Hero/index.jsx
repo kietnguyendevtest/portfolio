68a37766ebb6fdcb38e48c591d4c0f5fda6249a8
kietnguyendevtest <teiknguyen.work@gmail.com>
2024-02-20 22:35:37 +0700

    text run

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,9 +1,28 @@
-import React from "react";
-
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
 function Hero() {
+    const el = useRef(null);
+    const typed = useRef(null);
+
+    useEffect(() => {
+        const options = {
+            strings: ["Software Engineer", "Web Developer", "UI/UX Designer"],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true,
+        };
+        typed.current = new Typed(el.current, options);
+
+        return () => {
+            typed.current.destroy();
+        };
+    }, []);
+
     return (
-        <div id="home" style={{ height: "100vh" }}>
-            Hero
+        <div id="home" style={{ paddingTop: "100px", height: "100vh" }}>
+            <p style={{ fontSize: "4rem" }}>
+                I'm a <span ref={el}></span>
+            </p>
         </div>
     );
 }
