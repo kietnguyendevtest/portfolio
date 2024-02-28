bb87bec0338b5830c7230b22705d36a3c7fe7188
kietnguyendevtest <teiknguyen.work@gmail.com>
2024-02-28 16:45:12 +0700

    project page v2

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -6,14 +6,12 @@ function Projects() {
     return (
         <div id="projects" className="projects-container">
             <div className="container">
-                {/* <h2 className="hash-title">
-                <span>OPEN SOURCE PROJECTS</span>
-            </h2> */}
+                <h2 className="hash-title">
+                    <span>OPEN SOURCE PROJECTS</span>
+                </h2>
 
-                <br /> <br /> <br />
-
-                <ul>
-                    <li>
+                <div className="row row-cols-xxl-3 row-cols-lg-2 row-cols-md-1 gy-4">
+                    <div className="col">
                         <Card>
                             <a
                                 href="https://nmkiet.io.vn/"
@@ -21,7 +19,7 @@ function Projects() {
                                 rel="noreferrer"
                             >
                                 <CardContent>
-                                    <FontAwesomeIcon icon="fa-solid fa-code" />
+                                    <FontAwesomeIcon icon="fa-solid fa-computer" />
                                     <p>Grocery Mart</p>
                                     <span>Ecommerce website</span>
 
@@ -48,62 +46,221 @@ function Projects() {
                                 </CardHover>
                             </a>
                         </Card>
-                    </li>
-                    <li style={{ marginTop: "50px" }}>
-                        <a
-                            href="https://kietnm-landing-celobank.vercel.app/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Landing CeloBank
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://kietnm-landing-besnik.vercel.app/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Landing Besnik
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://kietnm-landing-dental-care.vercel.app/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Landing Dental Care
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://kietnm-landing-lesson.vercel.app/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Landing Lesson
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://kietnm-reactjs.vercel.app/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            CRUD ReactJs
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://kietnm-nextjs.vercel.app"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            CRUD NextJs
-                        </a>
-                    </li>
-                </ul>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <a
+                                href="https://kietnm-landing-celobank.vercel.app/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <CardContent>
+                                    <FontAwesomeIcon icon="fa-solid fa-computer" />
+                                    <p>CeloBank</p>
+                                    <span>Landing website</span>
+
+                                </CardContent>
+                                <CardHover>
+                                    <ul>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-toolbox" />
+                                            <span>Grocery Mart</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-regular fa-comment-dots" />
+                                            <span>Build and develop ecommerce&nbsp;website</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-code" />
+                                            <span>HTML, CSS, SCSS, JS</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-link" />
+                                            <i>www.nmkiet.io.vn</i>
+                                        </li>
+                                    </ul>
+                                </CardHover>
+                            </a>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <a
+                                href="https://kietnm-landing-besnik.vercel.app/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <CardContent>
+                                    <FontAwesomeIcon icon="fa-solid fa-computer" />
+                                    <p>Besnik</p>
+                                    <span>Landing website</span>
+
+                                </CardContent>
+                                <CardHover>
+                                    <ul>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-toolbox" />
+                                            <span>Grocery Mart</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-regular fa-comment-dots" />
+                                            <span>Build and develop ecommerce&nbsp;website</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-code" />
+                                            <span>HTML, CSS, SCSS, JS</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-link" />
+                                            <i>www.nmkiet.io.vn</i>
+                                        </li>
+                                    </ul>
+                                </CardHover>
+                            </a>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <a
+                                href="https://kietnm-landing-dental-care.vercel.app/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <CardContent>
+                                    <FontAwesomeIcon icon="fa-solid fa-computer" />
+                                    <p>Dental Care</p>
+                                    <span>Landing website</span>
+
+                                </CardContent>
+                                <CardHover>
+                                    <ul>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-toolbox" />
+                                            <span>Grocery Mart</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-regular fa-comment-dots" />
+                                            <span>Build and develop ecommerce&nbsp;website</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-code" />
+                                            <span>HTML, CSS, SCSS, JS</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-link" />
+                                            <i>www.nmkiet.io.vn</i>
+                                        </li>
+                                    </ul>
+                                </CardHover>
+                            </a>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <a
+                                href="https://kietnm-landing-lesson.vercel.app/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <CardContent>
+                                    <FontAwesomeIcon icon="fa-solid fa-computer" />
+                                    <p>Lesson</p>
+                                    <span>Landing website</span>
+                                </CardContent>
+                                <CardHover>
+                                    <ul>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-toolbox" />
+                                            <span>Grocery Mart</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-regular fa-comment-dots" />
+                                            <span>Build and develop ecommerce&nbsp;website</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-code" />
+                                            <span>HTML, CSS, SCSS, JS</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-link" />
+                                            <i>www.nmkiet.io.vn</i>
+                                        </li>
+                                    </ul>
+                                </CardHover>
+                            </a>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <a
+                                href="https://kietnm-reactjs.vercel.app/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <CardContent>
+                                    <FontAwesomeIcon icon="fa-solid fa-computer" />
+                                    <p>CRUD ReactJs</p>
+                                    <span>Demo website</span>
+                                </CardContent>
+                                <CardHover>
+                                    <ul>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-toolbox" />
+                                            <span>Grocery Mart</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-regular fa-comment-dots" />
+                                            <span>Build and develop ecommerce&nbsp;website</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-code" />
+                                            <span>HTML, CSS, SCSS, JS</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-link" />
+                                            <i>www.nmkiet.io.vn</i>
+                                        </li>
+                                    </ul>
+                                </CardHover>
+                            </a>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <a
+                                href="https://kietnm-nextjs.vercel.app"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <CardContent>
+                                    <FontAwesomeIcon icon="fa-solid fa-computer" />
+                                    <p>CRUD NextJs</p>
+                                    <span>Demo website</span>
+                                </CardContent>
+                                <CardHover>
+                                    <ul>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-toolbox" />
+                                            <span>Grocery Mart</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-regular fa-comment-dots" />
+                                            <span>Build and develop ecommerce&nbsp;website</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-code" />
+                                            <span>HTML, CSS, SCSS, JS</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon icon="fa-solid fa-link" />
+                                            <i>www.nmkiet.io.vn</i>
+                                        </li>
+                                    </ul>
+                                </CardHover>
+                            </a>
+                        </Card>
+                    </div>
+                </div>
             </div>
         </div>
     );
